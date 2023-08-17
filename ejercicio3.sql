17639603ce1b807119fe27914e805c4490bf0eab
velacarolina52@gmail.com <velacarolina52@gmail.com>
2023-08-17 09:46:53 -0300

    ejercicio3

---
+++ ejercicio3.sql
@@ -0,0 +1,5 @@
+--Mostrar el nombre y duración de las canciones de los discos de "Deep Purple"
+SELECT name, milliseconds
+FROM tracks t JOIN albums a ON t.albumId = a.albumId
+WHERE ArtistId = (SELECT ArtistId FROM Artists
+WHERE name = 'Deep Purple');
